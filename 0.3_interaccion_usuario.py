d0f19dc46ec71fa576f8efd5a2308b0be6e51999
AlexCarrero <147519472+AlexCarrero@users.noreply.github.com>
2023-10-24 08:12:59 -0500

    Add files via upload

---
+++ 0.3_interaccion_usuario.py
@@ -0,0 +1,23 @@
+#autores:Jean Sebastian Vigoya, Alex Santiago Carrero, 2749613, 17/10/2023
+
+#examenes medicos
+
+print(input("digite su nombre: "))
+a="nombre"
+print(input("digite su apellido: "))
+b="apellido"
+print(input("digite su profesion: "))
+c="profesion"
+print(input("digite su correo: "))
+d="correo"
+print(input("digite su fecha de nacimiento: "))
+e="fecha de nacimiento"
+print(input("digite su tipo de  sangre: "))
+f="tipo de sangre"
+
+
+
+
+
+
+
---
+++ 0.4_Calculadora_Ley_ohm.py
@@ -0,0 +1,7 @@
+#autores:Jean Sebastian Vigoya, Alex Santiago Carrero, 2749613, 17/10/2023
+#Calculadora Ley de ohm 
+print("Ingrese el valor del voltaje y la resistencia")
+v=int(input("voltaje ="))
+r=int(input("resistencia ="))
+CalcularCorriente=v/r
+print("Calcular corriente :" ,v, "/" ,r, "=" ,CalcularCorriente,)
