3707ef5f48f292490bd2bdba6765efa4a2699783
AlexCarrero <147519472+AlexCarrero@users.noreply.github.com>
2023-10-31 07:49:32 -0500

    Add files via upload

---
+++ 0.6_Ciclo_While.py
@@ -0,0 +1,20 @@
+#autores:Jean Sebastian Vigoya, Alex Santiago Carrero, 2749613, 17/10/2023
+#0.6_ciclo_while
+
+N=int(input("escriba un numero :"))
+aux1=int(input("parametro del ciclo while :")) 
+print ("los numeros digitados son ", N, aux1)
+
+N=1
+while N < aux1 :
+    valor= aux1**N
+    
+    print (N)
+    print ("fin")
+
+
+
+
+
+
+
